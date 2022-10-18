faaf5de7d62c7795a8e778813b714c9e5d302a4d
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-10-18 16:00:56 -0300

    01

---
+++ taskapp/src/App.jsx
@@ -0,0 +1,13 @@
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+
+function App() {
+  return (
+    <>
+      <TaskForm />
+      <TaskList />
+    </>
+  );
+}
+
+export default App;
